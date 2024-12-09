1659a182918ad7f4bffaa7791e72adfe59dcce46
Nahorniy Roman <nahornyi.r22@fpm.dnu.edu.ua>
2024-12-09 21:17:21 +0200

    Add project files.

---
+++ Lab6/Graph.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class Graph
+    {
+        private readonly int _vertices;
+        private readonly List<int>[] _adjacencyList;
+        private readonly int[,] _adjacencyMatrix;
+
+        public Graph(int vertices)
+        {
+            _vertices = vertices;
+            _adjacencyList = new List<int>[vertices];
+            _adjacencyMatrix = new int[vertices, vertices];
+
+            for (int i = 0; i < vertices; i++)
+                _adjacencyList[i] = new List<int>();
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            _adjacencyList[v].Add(w);
+            _adjacencyList[w].Add(v);
+
+            _adjacencyMatrix[v, w] = 1;
+            _adjacencyMatrix[w, v] = 1;
+        }
+
+        public void PrintAdjacencyMatrix()
+        {
+            Console.WriteLine("Adjacency Matrix:");
+            for (int i = 0; i < _vertices; i++)
+            {
+                for (int j = 0; j < _vertices; j++)
+                    Console.Write(_adjacencyMatrix[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintAdjacencyList()
+        {
+            Console.WriteLine("Adjacency List:");
+            for (int i = 0; i < _vertices; i++)
+            {
+                Console.Write(i + ": ");
+                foreach (var vertex in _adjacencyList[i])
+                    Console.Write(vertex + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public bool BFSFind(int startVertex, int targetVertex)
+        {
+            bool[] visited = new bool[_vertices];
+            Queue<int> queue = new Queue<int>();
+
+            visited[startVertex] = true;
+            queue.Enqueue(startVertex);
+
+            while (queue.Count > 0)
+            {
+                int vertex = queue.Dequeue();
+                Console.Write(vertex + " ");
+                if (vertex == targetVertex)
+                {
+                    return true;
+                }
+
+                foreach (var neighbor in _adjacencyList[vertex])
+                {
+                    if (!visited[neighbor])
+                    {
+                        visited[neighbor] = true;
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        public bool DFSFind(int startVertex, int targetVertex)
+        {
+            bool[] visited = new bool[_vertices];
+            return DFSFindUtil(startVertex, targetVertex, visited);
+        }
+
+        private bool DFSFindUtil(int vertex, int targetVertex, bool[] visited)
+        {
+            visited[vertex] = true;
+            Console.Write(vertex + " ");
+            if (vertex == targetVertex)
+            {
+                return true;
+            }
+
+            foreach (var neighbor in _adjacencyList[vertex])
+            {
+                if (!visited[neighbor])
+                {
+                    if (DFSFindUtil(neighbor, targetVertex, visited))
+                    {
+                        return true;
+                    }
+                    
+                }
+            }
+            return false;
+        }
+
+        public void PrimMST()
+        {
+            int[] parent = new int[_vertices];
+            int[] key = new int[_vertices];
+            bool[] mstSet = new bool[_vertices];
+
+            for (int i = 0; i < _vertices; i++)
+            {
+                key[i] = int.MaxValue;
+                mstSet[i] = false;
+            }
+
+            key[0] = 0;
+            parent[0] = -1;
+
+            for (int count = 0; count < _vertices - 1; count++)
+            {
+                int u = MinKey(key, mstSet);
+                mstSet[u] = true;
+
+                for (int v = 0; v < _vertices; v++)
+                {
+                    if (_adjacencyMatrix[u, v] != 0 && !mstSet[v] && _adjacencyMatrix[u, v] < key[v])
+                    {
+                        parent[v] = u;
+                        key[v] = _adjacencyMatrix[u, v];
+                    }
+                }
+            }
+
+            PrintMST(parent);
+        }
+
+        private int MinKey(int[] key, bool[] mstSet)
+        {
+            int min = int.MaxValue, minIndex = -1;
+
+            for (int v = 0; v < _vertices; v++)
+            {
+                if (!mstSet[v] && key[v] < min)
+                {
+                    min = key[v];
+                    minIndex = v;
+                }
+            }
+
+            return minIndex;
+        }
+
+        private void PrintMST(int[] parent)
+        {
+            Console.WriteLine("Minimum Spanning Tree (using Prim's algorithm):");
+            for (int i = 1; i < _vertices; i++)
+                Console.WriteLine($"{parent[i]} - {i}");
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,52 @@
+﻿using Lab6;
+using System;
+Console.WriteLine("Print number of vertices: ");
+int v = int.Parse(Console.ReadLine());
+Graph graph = new Graph(v);
+int first, second;
+for (int i = 0; i < v; i++)
+{
+    Console.WriteLine("Print first vertix: ");
+    first = int.Parse(Console.ReadLine());
+    Console.WriteLine("Print second vertix: ");
+    second = int.Parse(Console.ReadLine());
+    graph.AddEdge(first, second);
+}
+while (true)
+{
+    Console.WriteLine("1 - Print adjacency matrix, 2 - Print adjacency list, 3 - BFS, 4 - DFS, 5 - Prim MST");
+    int option;
+    if (int.TryParse(Console.ReadLine(), out option))
+    {
+        int start, target;
+        switch (option)
+        {
+            case 1:
+                graph.PrintAdjacencyMatrix();
+                break;
+            case 2:
+                graph.PrintAdjacencyList();
+                break;
+            case 3:
+                Console.WriteLine("Write start vertix: ");
+                start = int.Parse(Console.ReadLine());
+                Console.WriteLine("Write vertix to find: ");
+                target = int.Parse(Console.ReadLine());
+                graph.BFSFind(start, target);
+                Console.WriteLine();
+                break;
+            case 4:
+                Console.WriteLine("Write start vertix: ");
+                start = int.Parse(Console.ReadLine());
+                Console.WriteLine("Write vertix to find: ");
+                target = int.Parse(Console.ReadLine());
+                graph.DFSFind(start, target);
+                Console.WriteLine();
+                break;
+            case 5:
+                graph.PrimMST();
+                break;
+        }
+    }
+
+}
